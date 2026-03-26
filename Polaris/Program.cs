9457f0587f0300f9ac6582b2ff7f5c2233a50e67
BRIAN EDUARDO CRUZ OLMEDO <brianeduardo1013@gmail.com>
2026-03-25 19:30:30 -0500

    mejorar experiencia de usuario (UX) en formulario POLARIS

--- Polaris/Program.cs
+++ Polaris/Program.cs
@@ -1,4 +1,5 @@
 using Polaris.Components;
+using Polaris.Components.Pages;
 using Polaris.Servicio;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddScoped<FondoVideoService>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
---
+++ Polaris/Servicio/FondoVideoService.cs
@@ -0,0 +1,28 @@
+﻿using Polaris.Data;
+
+namespace Polaris.Servicio
+{
+    public class FondoVideoService
+    {
+        public TiposProyectos tiposProyectos { get; set; } = TiposProyectos.PolariX;
+        public string linkVideo() => linksVideo[(int)tiposProyectos];
+
+        public event Action OnChange;
+
+        public void UpdateData(TiposProyectos newData)
+        {
+            tiposProyectos = newData;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        
+
+        public List<string> linksVideo { get; set; } = new() {
+            "https://www.youtube.com/embed/wnhvanMdx4s?autoplay=1&mute=1&controls=0&loop=1",
+            "https://www.youtube.com/embed/nGnX6GkrOgk?autoplay=1&mute=1&controls=0&loop=1",
+            "https://www.youtube.com/embed/Jk88_BaEi00?autoplay=1&mute=1&controls=0&loop=1"
+        };
+
+    }
+}
