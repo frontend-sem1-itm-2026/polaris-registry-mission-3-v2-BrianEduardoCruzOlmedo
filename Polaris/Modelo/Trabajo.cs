3c07073713088b9df526a027524f03d564780f58
BRIAN EDUARDO CRUZ OLMEDO <brianeduardo1013@gmail.com>
2026-04-20 18:12:01 -0500

    feat: implementar vista de gestión de trabajos

---
+++ Polaris/Modelo/Trabajo.cs
@@ -0,0 +1,18 @@
+﻿namespace Polaris.Modelo
+{
+    public class Trabajo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string TipoTrabajo { get; set; }
+
+        public List<string> Participantes { get; set; } = new();
+        public string? Descripcion { get; set; }
+        public DateTime FechaInicioTrabajo { get; set; } = DateTime.Now;
+        public DateTime FechaInicioRegistro { get; set; } = DateTime.Now;
+        public DateTime? FechaFinTrabajo { get; set; } = DateTime.Now.AddDays(7);
+        public DateTime? FechaFinRegistro { get; set; } = DateTime.Now.AddDays(3);
+
+    }
+}
